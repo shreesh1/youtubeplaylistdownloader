7e81d3522a805397fd00e1917fb310ffe51d8992
Shreesh Roliwal <sroliwal@gmail.com>
2020-01-09 05:59:34 -0500

    Update ytplaylist.sh

--- ytplaylist.sh
+++ ytplaylist.sh
@@ -1,1 +1,1 @@
-python3 links.py | grep -o 'watch.*' | cut -d '&' -f 1 | sed 's/^/https:\/\/www.youtube.com\//'
+python3 links.py $1 | grep -o 'watch.*' | cut -d '&' -f 1 | sed 's/^/https:\/\/www.youtube.com\//'
